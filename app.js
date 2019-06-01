df439e572a033c3fc3c9ce2e0dba3039eb8783f2
Nick Tsoukas <nick.tsoukas101@gmail.com>
2019-06-01 16:44:45 -0500

    Understanding how node can require and export files ... allowing devs to break up code into modules for readability and a better developement process

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-const fs = require('fs');
-const notes = ['this is note 1 ', 'this is note 2', 'this is note 3'];
+const object = require('./utils.js');
 
-fs.writeFileSync('notes.txt', 'Lets see what happens when we write to the file again');
+console.log(object.name, object.likesDog)
 
-notes.forEach((note) => {
 
-    fs.appendFile('notes.txt', `\n${note}`, (err) => {
-        if(err) return err;
-        console.log('adding a note to the file');
-        fs.readFile('notes.txt', (err, data) => {
-            let buf = new Buffer.from(data);
-            console.log(buf.toString('utf8'))
-        })
-    })
-})
